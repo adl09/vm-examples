27887cf14bbe447f0b8cdfaebefb0dad1a15d0fd
adl09 <adlange09@gmail.com>
2025-06-12 10:20:37 -0300

    Update VM guest command line parameters for consistency across configurations

--- apps/x86/zmq_samples/app_settings.cmake
+++ apps/x86/zmq_samples/app_settings.cmake
@@ -9,3 +9,6 @@ cmake_minimum_required(VERSION 3.8.2)
 # Define kernel config options
 set(KernelSel4Arch x86_64 CACHE STRING "" FORCE)
 set(KernelMaxNumNodes 1 CACHE STRING "" FORCE)
+
+set(SIMULATION OFF CACHE BOOL "Include only simulation compatible tests")
+set(RELEASE OFF CACHE BOOL "Performance optimized build")
